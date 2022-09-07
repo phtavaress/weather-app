e419e52739d7615f329a421f02f017fb1737caaf
Pedro Henrique <pedrohenrique@MacBook-Air-de-Pedro.local>
2022-09-07 19:31:57 -0300

    Making HTTP requests

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-console.log('Starting');
-setTimeout(() => {
-    console.log('2 secs');
-}, 2000);
-console.log('Stopping');
+const request = require('request');
+
+const url = 'http://api.weatherstack.com/current?access_key=f9132c480e212f522387bd03861b3401&query=37.8267,-122.4233';
+
+request({url:url, json: true}, (error, response) => {
+    const weatherDescription = response.body.current.weather_descriptions[0];
+    const temperature = response.body.current.temperature;
+    const feelsLike = response.body.current.feelslike;
+    console.log(`${weatherDescription}. It's currently ${temperature} degrees out. It feels like ${feelsLike} degrees out.`);
+})
